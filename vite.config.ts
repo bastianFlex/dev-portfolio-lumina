62e7d06755bd5ea3c99034a2f8e207a707242a51
bastianFlex <bastiansantosjr@gmail.com>
2025-10-25 10:03:40 -0400

    Adicionar configuração Vercel e corrigir base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? "/dev-portfolio-lumina/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
